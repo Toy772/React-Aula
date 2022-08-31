ecb0ab9fb9867b9e038d6fdee842c8036f81b3f6
vagner Roxo <vagnerroxo@hotmail.com>
2022-08-30 21:59:51 -0300

    Stateful

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
+class App extends Component{
+constructor(props){
+  super(props);
+  this.handlePclick = this.handlePclick.bind(this);
+  this.state = {
+    name: 'Vagner Roxo.',
+    counter: 0
+  };
+}
+
+  handlePclick(){
+    this.setState({name: 'Junior'})
+  }
+
+  handleAclick = (event) => {
+    event.preventDefault();
+    const {counter} = this.state;
+    this.setState({counter: counter + 1});
+  }
+
+  render(){
+
+    const {name, counter} = this.state;
+
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className= 'App-logo' alt='logo' ></img>
+          <p onClick={this.handlePclick}>
+            {name} {counter}
+          </p>
+          <a onClick={this.handleAclick} className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          >
+            Este é o Link
+          </a>
+        </header>
+
+      </div>
+    )
+  }
+}
+
+/* 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Olá Mundo!
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -20,6 +67,6 @@ function App() {
       </header>
     </div>
   );
-}
+} */
 
 export default App;
