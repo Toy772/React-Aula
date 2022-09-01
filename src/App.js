49f053a322890d50c671abb54edcdc1f05f1acb3
vagner Roxo <vagnerroxo@hotmail.com>
2022-08-31 22:29:26 -0300

    Ciclos de Vida

--- src/App.js
+++ src/App.js
@@ -4,31 +4,57 @@ import { Component } from 'react';
 class App extends Component{
 
   state = {
+    counter:0,
    posts:[
-    {
-      id:1,
-      title: 'O titulo 1',
-      body: 'O corpo 1'
-    },
-    {
-      id:2,
-      title: 'O titulo 2',
-      body: 'O corpo 2'
-    },
-    {
-      id:3,
-      title: 'O titulo 3',
-      body: 'O corpo 3'
-    },
-  ]
+      {
+        id:1,
+        title: 'O titulo 1',
+        body: 'O corpo 1'
+      },
+      {
+        id:2,
+        title: 'O titulo 2',
+        body: 'O corpo 2'
+      },
+      {
+        id:3,
+        title: 'O titulo 3',
+        body: 'O corpo 3'
+      },
+    ]
   };
+
+
+  timeOutUpdate = null;
+
+  componentDidMount(){
+    this.handleTimeOut();
+  }
+
+  componentDidUpdate(){
+    this.handleTimeOut();
+  }
+
+  componentWillUnmount(){
+    clearTimeout(this.timeOutUpdate)
+  }
+
+  handleTimeOut = () =>{
+    const {posts,counter} =this.state;
+    posts[0].title = 'O titulo Mudou';
+    
+    this.timeOutUpdate =  setTimeout(() => {
+      this.setState({posts,counter: counter + 1})  
+    }, 1000);
+  }
 
   render(){
 
-    const {posts} = this.state;
+    const {posts,counter} = this.state;
 
     return (
       <div className='App'>
+        <h1>{counter}</h1>
         {posts.map(posts =>
           (
             <div key={posts.id}>
